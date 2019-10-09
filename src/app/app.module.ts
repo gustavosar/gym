f24c3d63dea9cdbfe9d8c3aae01bf49d7bf679bf
Ramon Diniz de Alcantara Marinho <ramon.diniz@spassu.com>
2019-10-09 14:43:03 -0300

    Git add firebase and firestore service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+     IonicModule.forRoot(),
+     AppRoutingModule,
+     AngularFireModule.initializeApp(environment.firebase),
+     AngularFirestoreModule,
+     AngularFireAuthModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAK0cSoMDS1LG4ZHNsIdqRf9xW30NYyUTk",
+    authDomain: "gymapp-641dd.firebaseapp.com",
+    databaseURL: "https://gymapp-641dd.firebaseio.com",
+    projectId: "gymapp-641dd",
+    storageBucket: "gymapp-641dd.appspot.com",
+    messagingSenderId: "689040915220",
+    appId: "1:689040915220:web:4c521f42db5ddee5aae001",
+    measurementId: "G-12LKZTMN5Z"
+   }
 };
