c3a5c3bc80829ba5438821a25c8b6da8905c9b9c
Gustavo <gustavo.sa@spassu.com>
2019-10-09 18:33:02 -0300

    crud

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,7 +1,50 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { TaskI } from '../models/task.interface';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+  })
 
-export class FireStore {
+export class FirestoreService {
+
+    private todosCollection: AngularFirestoreCollection<TaskI>;
+    private todos: Observable<TaskI[]>;
   
-}
+    constructor(db:AngularFirestore) { 
+      this.todosCollection = db.collection<TaskI>('todos');
+      this.todos = this.todosCollection.snapshotChanges().pipe(
+        map(actions => {
+          return actions.map(a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return {id, ...data};
+          });
+        })
+      );
+    }
+  
+    getTodos(){
+      return this.todos;
+    }
+  
+    getTodo(id: string){
+      return this.todosCollection.doc<TaskI>(id).valueChanges();
+    }
+  
+    updateTodo(todo:TaskI, id: string){
+      return this.todosCollection.doc(id).update(todo);
+    }
+    
+    addTodo(todo: TaskI){
+      return this.todosCollection.add(todo);
+    }
+    
+    removeTodo(id: string){
+      return this.todosCollection.doc(id).delete();
+    }
+  
+  }
+  
--- src/app/tarefas/tarefas.page.ts
+++ src/app/tarefas/tarefas.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController, NavController } from '@ionic/angular';
+import { TaskI } from '../models/task.interface';
+import { FirestoreService } from '../services/firestore.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tarefas',
@@ -8,14 +11,31 @@ import { AlertController } from '@ionic/angular';
 })
 export class TarefasPage {
   public today = Date.now();
+  todos: TaskI[];
+  todoId= null;
 
-  constructor(public alertController: AlertController) {}
+  todo: TaskI = {
+    task: ''
+  };
+
+  constructor(
+    public alertController: AlertController, 
+    private firestoreService: FirestoreService,
+    private loadingController: LoadingController,
+    private route: ActivatedRoute,
+    private nav: NavController
+    ) {}
   
+  /* Alert */
   async presentAlert() {
     const alert = await this.alertController.create({
       header: 'Adicionar',
-      message: '<input class="tarefa-text" type="text">',
-      buttons: ['OK'],
+      message: '<input class="tarefa-text" [(ngModel)]="todo.task" type="text">',
+      buttons: [{
+        text: 'OK',
+        handler: () => { this.saveTodo() },
+
+      }],
     });
   
     await alert.present();
@@ -23,5 +43,53 @@ export class TarefasPage {
     console.log(result);
   }
 
+  /* Serviço Firestore */
+  ngOnInit(){
+    this.firestoreService.getTodos().subscribe((todos) =>{
+      this.todos = todos;
+    });
+
+    this.todoId = this.route.snapshot.params['id'];
+    if (this.todoId){
+      this.loadTodo();
+    }
+  }
+
+  /* Loading */
+  async loadTodo(){
+    const loading = await this.loadingController.create({
+      message: 'Loading....'
+    });
+    await loading.present();
+
+    this.firestoreService.getTodo(this.todoId).subscribe(todo => {
+      loading.dismiss();;
+      this.todo = todo;
+    });
+  }
+
+  /* Salvando Tarefa */
+  async saveTodo() {
+    const loading = await this.loadingController.create({
+      message: 'Salvando ...'
+    });
+    await loading.present();
+ 
+    if (this.todoId) {
+      this.firestoreService.updateTodo(this.todo, this.todoId).then(() => {
+        loading.dismiss();
+      });
+    } else {
+      this.firestoreService.addTodo(this.todo).then(() => {
+        loading.dismiss();
+      });
+    }
+  }
+
+
+  /* Deletando */
+  async onRemoveTodo(idTodo:string) {
+    this.firestoreService.removeTodo(idTodo);
+  }
 
 }
