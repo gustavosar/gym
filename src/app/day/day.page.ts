6a378152db00edf1de0dcb93fdb59e65f2e4300e
Gustavo <gustavo.sa@spassu.com>
2019-10-16 13:28:45 -0300

    load task inside day

--- src/app/day/day.page.ts
+++ src/app/day/day.page.ts
@@ -1,45 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { PickerController } from '@ionic/angular';
+import { FirestoreService } from '../services/firestore.service';
+import { TaskI } from '../models/task.interface';
+
 
 @Component({
   selector: 'app-day',
   templateUrl: './day.page.html',
   styleUrls: ['./day.page.scss'],
 })
-export class DayPage implements OnInit {
-
+export class DayPage implements OnInit{
   public today = Date.now();
-
-  constructor(private pickerCtrl: PickerController) { }
-
-  ngOnInit() {}
+  todos: TaskI[];
 
 
-  async openPicker() {
-    const picker = await this.pickerCtrl.create({
-      buttons: [{
-        text: 'Confirmar',
-        handler: (value: any): void => { console.log(value, 'ok'); }
-      },
-      {
-        text: 'Cancelar',
-        handler: (value: any): void => { console.log(value, 'cancel'); }
-      }],
-      columns: [
-        {
-          name: 'tarefas',
-          options: [
-            {
-              text: 'Ombro',
-            },
-          ]
-        }
-      ]
+  constructor(
+    private firestoreService: FirestoreService,
+    ) { }
+
+  ngOnInit(){
+
+    /* SERVICO */
+    this.firestoreService.getTodos().subscribe((todos) =>{
+      this.todos = todos;
     });
-    await picker.present();
+
   }
-
-
 
 
 }
